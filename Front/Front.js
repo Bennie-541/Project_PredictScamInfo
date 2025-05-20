e945b30045e55428719f9d6a4c0a83a3d53cfba2
Bennie <op71035@gmail.com>
2025-05-20 11:22:45 +0800

    更新首頁 HTML 標題與修正前端按鈕事件

--- Front/Front.js
+++ Front/Front.js
@@ -1,1 +1,7 @@
-const submit_Btn = document.getElementsBytypr
+const submit_Btn = document.getElementById("submitBtn")
+const reset_Btn = document.getElementByid("resetBtn");
+const inputElement = document.getElementByid("inputText");
+
+
+//送出資料按鈕事件
+submit_Btn.addEventListener
